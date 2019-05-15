8ab0a1f0900123d9e851cc33c1c4f26ddfd54075
Gangyuan <296671396@qq.com>
2019-05-15 14:51:16 +0800

    添加功能

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-var n = 1
-$('div').on('click', function(e) {
-  setTimeout(function() {
-    $(e.currentTarget).addClass('active')
-  }, n * 500)
-  n += 1
-})
+let divs = $('div').get()
+let n = 0
+for(let i=0; i<divs.length; i++){
+	divs[i].addEventListener('click', function(){
+		setTimeout(function() {
+			divs[i].classList.add('active')
+		}, n * 500)
+		n += 1
+	}, true) // 由外向里触发
+}
+for(let i=0; i<divs.length; i++){
+	divs[i].addEventListener('click', function(){
+		setTimeout(function(){
+			divs[i].classList.remove('active')
+		}, n * 500)
+		n += 1
+	}, false) // 由里向外触发
+}
+
